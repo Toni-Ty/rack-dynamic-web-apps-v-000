44e9bb893998d2fd2428b360d0e744b58522bdcb
Toni-Ty <antonique2479@yahoo.com>
2019-09-03 03:03:03 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -2,6 +2,11 @@ class Application
 
   def call(env)
     resp = Rack::Response.new
+
+    num 1 = Kernel.rand(1..20)
+    num 2 = Kernel.rand(1..20)
+    num 3 = Kernel.rand(1..20)
+
     resp.write "Hello, World"
     resp.finish
   end
